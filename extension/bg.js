9ae8dba5014055c8bcb35310bc6b78379bedc718
bearzx <bearzx@gmail.com>
2018-07-23 16:28:15 -0400

    nah

--- extension/bg.js
+++ extension/bg.js
@@ -1,1 +1,41 @@
 console.log('yo from bg');
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    // console.log(msg);
+    if (msg.action == 'save-script') {
+        save_script(msg);
+    }
+});
+
+function save_script(msg) {
+    // console.log(`saving new script ${msg.name}`);
+    // console.log(msg.code);
+    chrome.storage.sync.get(['saved_scripts'], function(result) {
+        if ($.isEmptyObject(result)) {
+            chrome.storage.sync.set({ saved_scripts: { [msg.name]: msg.code } });
+        } else {
+            result.saved_scripts[msg.name] = msg.code;
+            chrome.storage.sync.set({ saved_scripts: result.saved_scripts }, function() {
+                chrome.storage.sync.get(['saved_scripts'], function(result) {
+                    console.log(result);
+                });
+            });
+        }
+    });
+}
+
+function show_code() {
+    console.log('querying all saved scripts ...');
+    chrome.storage.sync.get(['saved_scripts'], function(result) {
+        console.log(result.saved_scripts);
+    });
+}
+
+function rm_code(name) {
+    chrome.storage.sync.get(['saved_scripts'], function(result) {
+        delete result.saved_scripts[name];
+        chrome.storage.sync.set({ saved_scripts: result.saved_scripts }, function() {
+
+        });
+    });
+}
--- extension/panel.js
+++ extension/panel.js
@@ -1,3 +1,22 @@
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/chrome");
-editor.session.setMode("ace/mode/javascript");
+$(document).ready(function() {
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chrome");
+    editor.session.setMode("ace/mode/javascript");
+
+    editor.commands.addCommand({
+        name: 'save',
+        bindKey: { win: 'Ctrl-S', mac: 'Command-S' },
+        exec: (_editor) => {
+            // console.log($('#script-name').val());
+            // console.log(_editor.getValue());
+            let msg = {
+                action: 'save-script',
+                name: $('#script-name').val(),
+                code: _editor.getValue()
+            };
+            chrome.runtime.sendMessage(msg, function(response) {
+                console.log(response);
+            });
+        }
+    });
+});
