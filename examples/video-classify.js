8288d57b35e63764183da30287748903feaf75fd
bearzx <bearzx@gmail.com>
2019-01-31 15:02:18 -0500

    examples

---
+++ examples/video-classify.js
@@ -0,0 +1,25 @@
+// Grab elements, create settings, etc.
+const video = document.getElementById('video');
+
+// Create a webcam capture
+// navigator.mediaDevices.getUserMedia({ video: true })
+//   .then((stream) => {
+//     video.srcObject = stream;
+//     video.play();
+//   })
+
+// var stream = stream.captureStream();
+video.play();
+
+// Initialize the Image Classifier method with MobileNet passing the video as the
+// second argument and the getClassification function as the third
+ml5.imageClassifier('MobileNet', video).then(classifier => loop(classifier));
+
+const loop = (classifier) => {
+  classifier.predict()
+    .then(results => {
+      result.innerText = results[0].className;
+      probability.innerText = results[0].probability.toFixed(4);
+      loop(classifier) // Call again to create a loop
+    })
+};
