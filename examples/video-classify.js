a8bf2858acffdffc0b1c7d02dc975020334d33dd
bearzx <bearzx@gmail.com>
2019-02-11 23:17:00 -0500

    bla

--- examples/video-classify.js
+++ examples/video-classify.js
@@ -15,13 +15,15 @@ video.play();
 // second argument and the getClassification function as the third
 ml5.imageClassifier('MobileNet', video).then(classifier => loop(classifier));
 
-// loop = (classifier) => {
-//   classifier.predict()
-//     .then(results => {
-//       console.log(results[0].className);
-//       loop(classifier) // Call again to create a loop
-//     })
-// }
+loop = (classifier) => {
+  classifier.predict()
+    .then(results => {
+      _$('#bla').innerText = results[0].className;
+      if (!_video_.paused) {
+        loop(classifier) // Call again to create a loop
+      }
+    })
+}
 
 const loop = (classifier) => {
   classifier.predict()
