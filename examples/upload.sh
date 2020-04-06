5e3c1403247d33bd18655426a3ca9739c7f3a9d6
bearzx <bearzx@gmail.com>
2020-04-06 15:05:52 -0400

    cleaning

--- examples/upload.sh
+++
@@ -1,8 +0,0 @@
-server=bearzx@206.189.204.243:/var/www/bearzx.com/pgxz3
-# scp playground.html $server
-# scp -r imgs $server
-# scp movie.mp4 $server
-# scp camera.html $server
-# scp camera.js $server
-scp animals.html $server
-# scp -r animals $server
