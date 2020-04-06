ad3b89240f7cd0c53d4153a8eed245586dab5cb1
bearzx <bearzx@gmail.com>
2020-04-06 16:09:04 -0400

    js dependencies cleanup

---
+++ build/precache-manifest.e66b91cdf5e389ffc84530c309f030a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc236865f4fc48bd639e84873265cc1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1296ef854c445dd00ce8",
+    "url": "/static/css/main.b9c820d7.chunk.css"
+  },
+  {
+    "revision": "497fa82595dedd1987f5",
+    "url": "/static/js/2.4ad4ef30.chunk.js"
+  },
+  {
+    "revision": "7c4d099dd7c3188452a65a3c8c971f2d",
+    "url": "/static/js/2.4ad4ef30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1296ef854c445dd00ce8",
+    "url": "/static/js/main.17d49d35.chunk.js"
+  },
+  {
+    "revision": "595a902b58fecc5ff2a1",
+    "url": "/static/js/runtime-main.2695f9b1.js"
+  }
+]);
